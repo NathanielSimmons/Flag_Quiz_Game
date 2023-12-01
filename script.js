5d549bc5834fc50078ac48c189d589626182d61c
NathanielSimmons <nathaniel.cj.simmons@gmail.com>
2023-12-01 15:22:48 -0500

    Added beta questions

---
+++ script.js
@@ -0,0 +1,37 @@
+const flags=[
+    {
+        flagPath: 'flags/ad.png',
+        capital: 'Andorra la Vella',
+        continent: 'Europe',
+        correctAnswer: 'Andorra',
+        wrongAnswer: ['Albania', 'Romania']
+    },
+    {
+        flagPath: 'flags/ae.png',
+        capital: 'Abu Dhabi',
+        continent: 'Asia',
+        correctAnswer: 'United Arab Emirates',
+        wrongAnswer: ['Oman', 'Saudi Arabia']
+    },
+    {
+        flagPath: 'flags/af.png',
+        capital: 'Kabul',
+        continent: 'Asia',
+        correctAnswer: 'Afghanistan',
+        wrongAnswer: ['Turkmenistan', 'Pakistan']
+    },
+    {
+        flagPath: 'flags/ag.png',
+        capital: 'St. Johns',
+        continent: 'North America',
+        correctAnswer: 'Antigua & Barbuda',
+        wrongAnswer: ['Guyana', 'Barbados']
+    },
+    {
+        flagPath: 'flags/ai.png',
+        capital: 'The Valley',
+        continent: 'North America',
+        correctAnswer: 'Anguilla',
+        wrongAnswer: ['Cayman Islands', 'British Virgin Islands']
+    },
+]
